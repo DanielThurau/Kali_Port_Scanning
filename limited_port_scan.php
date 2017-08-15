7683d2d96ad040ee7cf4f6863bf1f22f8006fefe
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-08-15 10:30:25 -0700

    open ports back to subject

--- limited_port_scan.php
+++ limited_port_scan.php
@@ -614,9 +614,9 @@ function send_email() {
     $mail = new PHPMailer;
 
     if($actionable_count > 0){
-	    $mail->Subject = 'ACTION REQUIRED: Scan Results from Kali on ' . $date . '. There are ' . $actionable_count . ' actionable events, and ' . $machineCount . ' peripherals scanned.';  
+	    $mail->Subject = 'ACTION REQUIRED: Scan Results from Kali on ' . $date . '. There are ' . $open . ' actionable events, and ' . $machineCount . ' peripherals scanned.';  
      }else{
-        	$mail->Subject = 'Scan Results from Kali on ' . $date . '. There are ' . $actionable_count . ' actionable events, and ' . $machineCount . ' peripherals scanned.'; 
+        	$mail->Subject = 'Scan Results from Kali on ' . $date . '. There are ' . $open . ' actionable events, and ' . $machineCount . ' peripherals scanned.'; 
      }
      $body = "Open Ports: " . $open . "\n";
     $body.= "Open|Filtered Ports: " . $openFiltered . "\n";
