99ae1bed920e4b2aba7e892ad02ac2d9ada15948
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-08-17 13:44:12 -0700

    Effcient way of sending emails for python. Still need to zip

--- Email.py
+++ Email.py
@@ -1,3 +1,40 @@
-class Email:
-  def __init__(self):
-    
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import os
+
+def sendMail(to, fro, subject, text, files=[],server="localhost"):
+    assert type(to)==list
+    assert type(files)==list
+
+
+    msg = MIMEMultipart()
+    msg['From'] = fro
+    msg['To'] = COMMASPACE.join(to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    for file in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(file,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"'
+                         % os.path.basename(file))
+        msg.attach(part)
+    try:
+        smtp = smtplib.SMTP(server)
+        smtp.sendmail(fro, to, msg.as_string() )
+        smtp.close()
+        print("Successfully sent mail")
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+
+
+
+# Example:
+sendMail(['Daniel <daniel.thurau@nbcuni.com>'],'Scanner <Scanner@KaliBox.com>','Hello Python!','Heya buddy! Say hello to Python! :)',['output-perimeter.csv'])
