95e919241e9116286706ef3f30bcf0f5a18df768
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-08-20 21:41:58 -0700

    docstring on log.py. Removed empty file i thought existed for a reason

--- log.py
+++ log.py
@@ -3,4 +3,5 @@ import datetime
 import time
 
 def send_log(message):
-            logging.info(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + " " + message)
+  """Logging method used throughout Kali_Port_Scanning"""
+  logging.info(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + " " + message)
