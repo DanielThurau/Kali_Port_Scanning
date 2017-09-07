f5da7a48fe9ffe029884c1cb41402e9b410912bd
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-09-07 14:23:23 -0700

    module moved out of the house at 18

--- KPS/Log.py
+++
@@ -1,7 +0,0 @@
-import logging
-import datetime
-import time
-
-def send_log(message):
-  """Logging method used throughout Kali_Port_Scanning"""
-  logging.info(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + " " + message)
--- limited_port_scan.py
+++ limited_port_scan.py
@@ -1,64 +1,16 @@
-#  Original Author : Rob McNeal
-#  limited_port_scan.php
-#
-#  Description:
-#  This script takes in configuration files that provide IP addresses and ports to
-#  be scanned. v1.0 uses nmap xml format, and generates web/email accessible reports
-#  for immediate notifcation. v1.1 was a fork that will eventually expand on the
-#  reporting system, There was a need at NBCUniversal of a cron-job-esque php
-#  script that could generate csv reports that reported on the status of select
-#  ports on provided IP addresses. v1.1 also uses nmap, and added concurrency because
-#  of the massive amount of scannable ports.
-# 
-#     ___________________________________________________________________________________________________________________________________________
-#    |    Version   |       Author         |  Release Date  |       Contact            |                       Comment                           |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#    |      1.0     |   Rob McNeal         |        ?       | sporac@hotmail.com       |  Original Release. Checkout orig branch.                |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#    |      1.1     |   Daniel Thurau      |     7-11-17    | Daniel.thurau@nbcuni.com |  First Deployable version inside of NBCU. CLI tool.     |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#    |      1.2     |   Daniel Thurau      |     7-18-17    | Daniel.thurau@nbcuni.com |  Email notification module.                             |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#    |      1.3     |   Daniel Thurau      |     7-19-17    | Daniel.thurau@nbcuni.com |  Email module extended heavily. Intro of flags          |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#    |      1.4     |   Daniel Thurau      |     8-14-17    | Daniel.thurau@nbcuni.com |  Converted to python                                    |
-#    |______________|______________________|________________|__________________________|_________________________________________________________|
-#
-#
-#   Usage:
-#       A makefile exists in the devel version and any developer should follow that guideline
-#       because of specific development env variables.
-#
-#           {python3} limited_port_scan.php [buisness_unit]
-#
-#        Refer to the manual for expanded explanation and for config folder setup.
-# 
+from portscan import BusinessUnit
 
+from portscan import Emailing
 
-
-import KPS.BusinessUnit
-print("made it here -1")
-import KPS.Emailing
-
-from KPS.Log import *
+from portscan import Log
 
 import argparse
 import os
 import sys
 
-print("made it here 0")
-
 FULL_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
-LOG_FILE = FULL_PATH + ".log"
 UNIX_LIKE = True
 BUSINESS_PATH="/scanning/Kali_Port_Scanning/external.csv"
-
-print("made it here 1")
-
-logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
-
-
-print("made it here 2")
 
 parser = argparse.ArgumentParser(description='Lets scan some ports')
 
@@ -75,8 +27,7 @@ if args.businessName:
 if args.org:
     org = args.org
 
-business_unit = KPS.BusinessUnit.BusinessUnit(args.business_unit, FULL_PATH, bs, org)
-
+business_unit = BusinessUnit.BusinessUnit(args.business_unit, FULL_PATH, bs, org)
 
 # At this point the object is substantiated and all dependencies have been resolved. 
 
@@ -90,9 +41,8 @@ business_unit.Collect(BUSINESS_PATH)
 
 
 if len(business_unit.emails) > 0 or len(business_unit.mobile) > 0:
-    KPS.Emailing.SendMail(business_unit)
+    Emailing.SendMail(business_unit)
 
 # Flush stdout if fork has failed
 sys.stdout = sys.__stdout__
 sys.stdout.flush()
-
